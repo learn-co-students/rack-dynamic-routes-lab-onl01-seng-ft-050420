d2cb9c52def924ed0c760bcc68360e60e430c4e7
github username <github email address>
2020-05-20 22:27:06 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
 
-      if item = @@items.find {|item| item.name = item_name}
+      if item = @@items.find {|item| item.name == item_name}
         resp.write "#{item.price}"
         resp.status = 200
       else
